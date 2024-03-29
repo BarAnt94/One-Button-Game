cac6d735d57cd24f188dabecadd8926ac39946c0
BarAnt94 <122675451+BarAnt94@users.noreply.github.com>
2024-03-29 16:01:47 +0100

    Béta Niveau 1

--- Assets/Script/C#_Script/Trigger_Plateforme.cs
+++ Assets/Script/C#_Script/Trigger_Plateforme.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Trigger_Plateforme : MonoBehaviour
 {
+    public Rigidbody rb;
     void FixedUpdate()
     {
         if (transform.position.y > GameObject.FindGameObjectWithTag("Player").transform.position.y)
         {
-            GetComponent<BoxCollider>().isTrigger = true;
+            GetComponent<BoxCollider>().isTrigger = true; 
         }
-        else
+        else 
         {
             GetComponent<BoxCollider>().isTrigger = false;
         }
